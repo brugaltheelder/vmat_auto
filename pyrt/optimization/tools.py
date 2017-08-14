19cf2453775a29723757789e505af391fbea0220
Troy Long <troy.long@utsouthwestern.edu>
2017-08-14 16:19:41 -0500

    aper dkj probably working

--- pyrt/optimization/tools.py
+++ pyrt/optimization/tools.py
@@ -9,12 +9,12 @@ class aperture(object):
         assert(isinstance(CP,control_point))
         assert (isinstance(data, patient_data))
         if set_open_aper:
-            self.left_leaves = [CP.left_leaf_position[r] for r in range(CP.num_rows)]
-            self.right_leaves = [CP.left_leaf_position[r] + CP.width_per_row[r] for r in range(CP.num_rows)]
+            self.left_leaf_position = [CP.left_leaf_position[r] for r in range(CP.num_rows)]
+            self.right_leaf_position = [CP.left_leaf_position[r] + CP.width_per_row[r] for r in range(CP.num_rows)]
             self.intensity = 1.
         else:
-            self.left_leaves = aper_left_pos[:]
-            self.right_leaves = aper_right_pos[:]
+            self.left_leaf_position = aper_left_pos[:]
+            self.right_leaf_position = aper_right_pos[:]
             self.intensity = aper_intensity
 
         self.build_Dkj(CP,data)
@@ -24,12 +24,15 @@ class aperture(object):
     def build_Dkj(self,CP,data):
         assert (isinstance(CP, control_point))
         assert (isinstance(data, patient_data))
+        self.Dkj_per_structure = [np.zeros(s.num_vox) for s in self.data.structures]
         for s in range(len(data.structures)):
             dense_dkj = np.zeros(data.structures[s].num_vox)
+            beamlet_indices = []
             for r in range(CP.num_rows):
-                for i in range(self.left_leaves[r], self.right_leaves[r]):
-                    beamlet_index = CP.left_leaf_index[i]
-            self.Dkj_per_structure = None
+                for i in range(self.left_leaf_position[r], self.right_leaf_position[r]):
+                    beamlet_indices.append(CP.initial_beamlet_index + CP.left_leaf_index[i] + (self.left_leaf_position[r] - CP.left_leaf_position[r]))
+
+            self.Dkj_per_structure = np.asarray(data.structures[s].Dij[np.array(beamlet_indices)].sum(axis=0))
 
 
     def calc_dose(self,x=None):
