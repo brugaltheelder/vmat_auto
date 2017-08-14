6218315000af7cb3b51be3fd7a870e93b56d4cd4
Troy Long <troy.long@utsouthwestern.edu>
2017-08-14 14:20:29 -0500

    started building vmat model, added skeleton of control point generation

--- pyrt/optimization/vmat.py
+++ pyrt/optimization/vmat.py
@@ -13,3 +13,47 @@ from pyrt.optimization.model import *
 
 
 
+
+class vmat_mip(model_base):
+    def __init__(self, input_dict,modality='imrt', run_title='default'):
+        super(self.__class__,self).__init__(input_dict,modality=modality)
+        self.run_title = run_title
+        self.model_params = input_dict['model_params'].copy()
+        self.build_model()
+
+
+
+    def build_model(self):
+        # generate optimization metadata you need, can break into functions like you did before (building vars, dose vars, aper vars, etc)
+        # you have self.data as the data object
+
+        pass
+
+
+
+
+
+    def optimize(self): # probably need some inputs/default params like in IMRT one
+
+
+        # write optimization code here (probably self.m.optimize(), then some data extraction)
+
+
+        # save apertures (or the indices of apertures) of solution
+
+        self.dose_dict[self.run_title] = [np.copy(self.current_dose_per_structure[s]) for s in range(len(self.data.structures))]
+        self.obj_dict[self.run_title] = self.current_obj
+
+
+    def calc_dose_from_variables(self):
+        # given apertures, calculate dose
+        #  will be useful in long run
+
+        pass
+
+
+
+
+    def calc_obj_grad(self, beamlet_intensities):
+
+        pass
