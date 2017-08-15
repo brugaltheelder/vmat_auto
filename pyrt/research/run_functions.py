763f554a492d0544b30b880e90e25bbc64d78940
jtmargo <jtmargo@clemson.edu>
2017-08-15 16:27:05 -0500

    added list of files to skip in run_functions for case specific

--- pyrt/research/run_functions.py
+++ pyrt/research/run_functions.py
@@ -5,43 +5,33 @@ from pyrt.tools import print_structure_info
 # Run All
 def run_all(case,input_dict):
     print_in_box('Running All Cases')
+
+    if case == 'Prostate_VMAT':
+        skip_files = [304,305]
+    else:
+        skip_files = []
+
     for filename in os.listdir(input_dict['cwd']):
 
-        if '10' in filename:
-            continue
 
-        if '201' in filename:
-            continue
 
-        if '202' in filename:
-            continue
-        if '203' in filename:
-            continue
-
-        if '204' in filename:
-            continue
-
-        if '205' in filename:
-            continue
-
-        if '304' in filename:
-            continue
 
         if case in filename:
+            if int(filename[-7:-4]) not in skip_files:
+                continue
             # build input dictionary for a particular case
             input_dict['filename'] = filename
 
             # call run_case(input)
 
             run_case(input_dict)
-            # print_structure_info(data)
             print '-' * 40
     print_in_box('All Cases Completed')
 
 
 def run_case(input_dict):
 
-    print_in_box('Now Running {}'.format(input_dict['filename']))
+    print_in_box('Now Running {}'.format(input_dict['filename']),1)
     # build model object
     model = vmat_mip(input_dict)
 
