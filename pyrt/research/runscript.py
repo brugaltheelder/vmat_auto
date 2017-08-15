fff7f71b1bfe9e1a7840576af083625d379647d1
jtmargo <jtmargo@clemson.edu>
2017-08-15 16:28:25 -0500

    added run script for individual file

---
+++ pyrt/research/runscript.py
@@ -0,0 +1,33 @@
+from pyrt.optimization.vmat import *
+from pyrt.research.run_functions import *
+
+# Josh Inputs - Prostate
+# Find work directory
+# cwd = '/Users/troy/Dropbox/CAP Group/TROTS/Prostate_VMAT/'
+working_directory = 'outputs_josh/'
+cwd = '/Users/jtmargo/Desktop/Josh/Clemson University/Research/Treatment Plan/TROTS Data/Prostate_VMAT/'
+
+vmat_model_params = {
+    'target_weights':{'PTV':1000., 'default':500.},
+    'oar_weights':{'Rectum':10., 'Bladder':10., 'default':1.},
+    'max_intensity':1000.,
+    'min_intensity': 0.,
+    'aper_limit': 1.
+}
+
+
+input_dict = {
+    'cwd': cwd,
+    'figure_directory':working_directory,
+    'filename': None,
+    'Rx': {'PTV': 79.56, 'PTV Vesicles': 72.2},
+    'model_params':vmat_model_params
+}
+
+
+#Run individual case
+
+# Load patient information
+input_dict['filename'] = 'Prostate_VMAT_306.mat'
+
+run_case(input_dict)
