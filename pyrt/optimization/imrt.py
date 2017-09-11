bd0d49a9144c2940c2efe4cbc3709c73f22418be
Troy Long <troy.long@utsouthwestern.edu>
2017-09-11 14:46:33 -0500

    started implementing tora imrt

--- pyrt/optimization/imrt.py
+++ pyrt/optimization/imrt.py
@@ -11,10 +11,19 @@ from pyrt.optimization.tools import *
 
 
 class imrt_fmo(model_base):
-    def __init__(self, input_dict,modality='imrt', run_title='default'):
+    def __init__(self, input_dict,modality='imrt', run_title='default', tora = False, tora_dose=None):
         super(self.__class__,self).__init__(input_dict,modality=modality)
         self.run_title = run_title
         self.model_params = input_dict['model_params'].copy()
+        self.tora = tora
+        if self.tora:
+            if tora_dose is None:
+                print 'SET TORA DOSE!'
+                exit()
+            else:
+                #todo write dose sorting function
+                self.tora_dose = [d.copy() for d in tora_dose]
+                self.sorted_target_dose_dict =  [np.ones(s.num_vox) for s in self.data.structures]
         self.build_model()
 
 
@@ -28,32 +37,83 @@ class imrt_fmo(model_base):
         # parameters
         # thresholds
         self.thresholds_per_structure = [np.ones(s.num_vox)*s.rx for s in self.data.structures]
+        if self.tora:
+            pass
+
+
         # weights
         self.weights_per_structure = None
-        save_weights_from_input_dict(self, target_weights_label=target_weights_label,oar_weights_label=oar_weights_label)
+        if self.tora:
+            self.gen_weights_tora()
+        else:
+            save_weights_from_input_dict(self, target_weights_label=target_weights_label,oar_weights_label=oar_weights_label)
+
+
+    def gen_tora_threshold(self, scalar = 0., sorted_index_dict = None):
+        self.threshold_primary_over = [np.ones(s.num_vox) for s in self.data.structures]
+        self.threshold_primary_under = [np.ones(s.num_vox) for s in self.data.structures]
+
+        # resort target_dose_dist
+        if sorted_index_dict is not None:
+            for s in range(self.data.structures):
+                if self.data.structures[s].is_target:
+                    self.threshold_primary_over[s][sorted_index_dict[s]] = np.maximum((1 - scalar) * self.sorted_target_dose_dict[
+                        s] + scalar * self.thresholds_per_structure[s][sorted_index_dict[s]],self.thresholds_per_structure[s][sorted_index_dict[s]])
+                    self.threshold_primary_under[s][sorted_index_dict[s]] = np.minimum((1 - scalar) * self.sorted_target_dose_dict[
+                        s] + scalar * self.thresholds_per_structure[s][sorted_index_dict[s]],self.thresholds_per_structure[s][sorted_index_dict[s]])
+                else:
+                    self.threshold_primary_over[s][sorted_index_dict[s]] = self.sorted_target_dose_dict[s]
+                    self.threshold_primary_under[s][sorted_index_dict[s]] = self.sorted_target_dose_dict[s]
+
+        else:
+            for s in range(self.data.structures):
+                self.threshold_primary_over[s] = (1-scalar) * self.tora_dose[s] + scalar * self.thresholds_per_structure[s]
+                self.threshold_primary_under[s] = (1-scalar) * self.tora_dose[s] + scalar * self.thresholds_per_structure[s]
 
 
 
-    def optimize(self,startingX=None, UB=None, display=False, ftol=1e-5, gtol=1e-5):
+
+
+
+    def gen_weights_tora(self, epsilon=0.001):
+        self.weights_per_structure = [np.ones(s.num_vox) for s in self.data.structures]
+        self.primary_weights_over  = [np.ones(s.num_vox) for s in self.data.structures]
+        self.primary_weights_under = [np.ones(s.num_vox) for s in self.data.structures]
+        for s_ind in range(len(self.data.structures)):
+            if self.data.structures[s_ind].is_target:
+                self.primary_weights_under[s_ind] *= 1./self.data.structures[s_ind].num_vox
+            else:
+                self.primary_weights_under[s_ind] *= 0.
+            self.primary_weights_over *= 1./self.data.structures[s_ind].num_vox
+
+            # meanStructureDoseDeviation = max(np.average((self.ideal_dose_dist[self.structure_index_dict[s]] -
+            #                                              self.target_dose_dist[self.structure_index_dict[s]]) ** 2), 1.)
+            #
+
+
+
+
+    def optimize(self,startingX=None, UB=None, display=False, ftol=1e-5, gtol=1e-5, tora=False):
         if startingX is not None:
             x0 = np.copy(startingX)
         else:
             x0 = np.zeros(self.data.num_beamlets)
 
+        if tora==False:
+            start = time()
+            res = spo.minimize(self.calc_obj_grad, x0=x0, method='L-BFGS-B', jac=True, bounds=[(0, UB) for i in
+                            xrange(self.data.num_beamlets)], options={'ftol': ftol, 'gtol': gtol, 'disp': display})
 
-        start = time()
-        res = spo.minimize(self.calc_obj_grad, x0=x0, method='L-BFGS-B', jac=True, bounds=[(0, UB) for i in
-                        xrange(self.data.num_beamlets)], options={'ftol': ftol, 'gtol': gtol, 'disp': display})
+            print '{} model solved in {} seconds'.format(self.modality,time() - start)
 
-        print '{} model solved in {} seconds'.format(self.modality,time() - start)
+            self.beamlet_intensities, self.current_obj =res['x'], res['fun']
+            self.calc_dose_from_variables()
 
-        self.beamlet_intensities, self.current_obj =res['x'], res['fun']
-        self.calc_dose_from_variables()
-
-
-
-        self.dose_dict[self.run_title] = [np.copy(self.current_dose_per_structure[s]) for s in range(len(self.data.structures))]
-        self.obj_dict[self.run_title] = self.current_obj
+            self.dose_dict[self.run_title] = [np.copy(self.current_dose_per_structure[s]) for s in range(len(self.data.structures))]
+            self.obj_dict[self.run_title] = self.current_obj
+        else:
+            # tora stuff
+            pass
 
 
     def calc_dose_from_variables(self,x=None):
@@ -88,3 +148,4 @@ class imrt_fmo(model_base):
             self.grad += self.data.structures[s].Dij.dot(self.zhat[s])
 
         return obj, self.grad
+
